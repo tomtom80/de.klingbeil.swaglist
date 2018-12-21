fbe653d9f7890acd8ceb227fdfa0d52fc3873409
tom <tom@toms-Air.fritz.box>
2018-12-21 22:58:08 +0100

    add firebase auth and facebook login

---
+++ wishlist-mobil/app/components/Splash/Splash.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import { View, Text, ActivityIndicator, Image } from 'react-native';
+
+import styles from './styles'
+
+export default class extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.wrapper}>
+                    <Image style={styles.image} source={{uri: "https://img.icons8.com/ios/100/000000/modern-razor.png"}}/>
+                    <Text style={styles.title}>A Project for Fun</Text>
+                </View>
+                <View style={styles.activityIndicatorContainer}>
+                    <ActivityIndicator animating={true}/>
+                </View>
+            </View>
+        );
+    }
+}
---
+++ wishlist-mobil/app/config/constants.js
@@ -0,0 +1,8 @@
+export const FIREBASE_API_KEY = "AIzaSyCda9wmj3mzzv7CWcIjwP94GgDSsovYPbM";
+export const FIREBASE_AUTH_DOMAIN = "wishlist-mobil.firebaseapp.com";
+export const FIREBASE_DATABASE_URL = "https://wishlist-mobil.firebaseio.com";
+export const FIREBASE_PROJECT_ID = "wishlist-mobil";
+export const FIREBASE_STORAGE_BUCKET = "wishlist-mobil.appspot.com";
+export const FIREBASE_MESSAGING_SENDER_ID = "690594768865";
+
+export const FACEBOOK_APP_ID = "222807195303889";

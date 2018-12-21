0614d29aa2414661bd56a2d38b8e024177da43ad
tom <tom@toms-Air.fritz.box>
2018-12-21 23:18:04 +0100

    cleanup icons

--- wishlist-mobil/app/modules/auth/scenes/Welcome/styles.js
+++ wishlist-mobil/app/modules/auth/scenes/Welcome/styles.js
@@ -22,7 +22,7 @@ const styles = StyleSheet.create({
     image:{
         height: 100,
         width: 100,
-        backgroundColor: color.white,
+        backgroundColor: "#FF553F",
         marginBottom: padding,
         resizeMode
     },

660cf6076a7cddd7e5a86d5b0a9c7510920422ca
tom <tom@toms-Air.fritz.box>
2019-07-11 10:40:54 +0200

    added all unittests module

---
+++ wishlist/unittests/src/test/java/de/klingbeil/wishlist/AllTestsSuite.java
@@ -0,0 +1,9 @@
+package de.klingbeil.wishlist;
+
+import org.junit.extensions.cpsuite.ClasspathSuite;
+import org.junit.runner.RunWith;
+
+@RunWith(ClasspathSuite.class)
+public class AllTestsSuite {
+
+}
